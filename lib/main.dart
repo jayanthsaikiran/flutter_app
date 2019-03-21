589a95fc0ae9392d6648f294f0e00610be9c27a7
jayanthsaikiran <jayanthsaikiran@gmail.com>
2019-03-21 19:09:48 +0530

    Learned constraints,scaffold and styles in text

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,49 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Container(
-    color: Colors.redAccent,
-    child: Center(
-      child: Text(
-        'Jayanth Saikiran',
-        textDirection: TextDirection.ltr,
-        textScaleFactor: 2,
-      ),
-    ),
+  runApp(MaterialApp(
+    title: 'Lol',
+    home: MyApp(),
   ));
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text(
+          'Lol',
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+        elevation: 4,
+      ),
+      body: Container(
+        color: Colors.white,
+        alignment: Alignment(0, 0),
+        child: Container(
+          color: Colors.green[400],
+          child: Center(
+            child: Text(
+              'Jayanth Sai kiran',
+              textDirection: TextDirection.ltr,
+              style: TextStyle(
+                fontSize: 34,
+                fontStyle: FontStyle.italic,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          constraints: BoxConstraints(
+              maxHeight: 100.0,
+              maxWidth: 500.0,
+              minWidth: 300.0,
+              minHeight: 100.0),
+        ),
+      ),
+    );
+  }
 }
