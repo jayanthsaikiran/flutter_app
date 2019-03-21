90c4e77ff671e8b7d46d4717c423c8205b41b726
jayanthsaikiran <jayanthsaikiran@gmail.com>
2019-03-21 18:19:17 +0530

    Day 0

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Container(
+    color: Colors.redAccent,
+    child: Center(
+      child: Text(
+        'Jayanth Saikiran',
+        textDirection: TextDirection.ltr,
+        textScaleFactor: 2,
+      ),
+    ),
+  ));
+}
